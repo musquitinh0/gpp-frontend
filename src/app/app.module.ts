3d7a953fc52b795b1dc2cffc9bd07919859ae9b5
Filipe Ramalho <filipe.silva@ccc.ufcg.edu.br>
2023-08-23 02:07:10 -0300

    adiciona animacao de loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import { LoginComponent } from './account/login/login.component';
 import { CreateAccountComponent } from './account/create-account/create-account.component';
 import { HomeComponent } from './layout/home/home.component';
 import { AuthenticationComponent } from './layout/authentication/authentication.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HomeScreenComponent } from './telas/home-screen/home-screen.component';
 import { PerfilComponent } from './telas/perfil/perfil.component';
 import { httpInterceptorProviders } from './http-interceptors';
 import { EditPerfilComponent } from './telas/edit-perfil/edit-perfil.component';
 import { AddDispositivoComponent } from './telas/add-dispositivo/add-dispositivo.component';
 import { PoliciaComponent } from './telas/policia/policia.component';
+import { SpinnerComponent } from './telas/commom/spinner/spinner.component';
+import { LoadingInterceptor } from './http-interceptors/loading.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -27,7 +30,8 @@ import { PoliciaComponent } from './telas/policia/policia.component';
     PerfilComponent,
     EditPerfilComponent,
     AddDispositivoComponent,
-    PoliciaComponent
+    PoliciaComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,11 @@ import { PoliciaComponent } from './telas/policia/policia.component';
     FormsModule,
     AppRoutingModule,
   ],
-  providers: [httpInterceptorProviders],
+  providers: [httpInterceptorProviders, 
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
